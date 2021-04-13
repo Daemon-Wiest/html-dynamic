3c3bcb850e44166d17f52531945a84da07c8f310
Daemon Wiest <wiestdaemon@gmail.com>
2021-04-13 09:28:31 -0700

    2nd commit

---
+++ js/dynamic.js
@@ -0,0 +1,19 @@
+let names = [
+"Bryan", "Celina", "Lance", "Andrea", "Jonathan", "Tyler", "Chas", "Dustin", "Pete",
+"Tom", "Will", "Daemon", "Jada", "Parker"
+];
+
+const loadDiv=() => {
+    let divCtrl = document.getElementById("ps");
+    divCtrl.innerHTML="";
+    for(let name of names){
+            let p = `<p>${name}</p>`;
+            divCtrl.innerHTML += p;
+    };
+}
+const addName= () => {
+    let nameCtrl=document.getElementById("newname");
+    let name = nameCtrl.value;
+    names.push(name);
+    loadDiv();
+}
